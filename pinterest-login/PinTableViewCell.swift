9ceee8a6fd235814b1ab675e89cfe13c55691332
sheafk <sheafureyking@gmail.com>
2017-03-31 12:51:38 -0400

    Update PinTableViewCell.swift

--- pinterest-login/PinTableViewCell.swift
+++ pinterest-login/PinTableViewCell.swift
@@ -2,7 +2,7 @@
 //  PinTableViewCell.swift
 //  pinterest-login
 //
-//  Created by Flatiron School on 3/30/17.
+//  Created by Shea Furey-King on 3/30/17.
 //  Copyright © 2017 sfk. All rights reserved.
 //
 
